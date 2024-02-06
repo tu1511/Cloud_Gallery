13a63f5211430c7bb0403f914f68d7cd7cc01d96
tu1511 <131641671+tu1511@users.noreply.github.com>
2024-02-06 23:39:26 +0700

    add alertMessage

--- frontend/src/views/UploadView.vue
+++ frontend/src/views/UploadView.vue
@@ -12,7 +12,9 @@
                   <div>
                     <label for="file-input" class="form-label inline-block mb-2 text-gray-700">Upload Image</label>
                     <input type="file" id="file-input" class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 border border-solid border-gray-300 rounded"
-                    accept="image/png, image/jpg, image/jpeg" />
+                    accept="image/png, image/jpg, image/jpeg"
+                    @change="handleFileChange($event)" 
+                    />
                   </div>
                 </div>
                 <button class="w-full px-6 py-2.5 bg-green-600 text-white font-medium text-xs rounded shadow-md"
@@ -22,6 +24,7 @@
           <!-- Image Preview -->
           <div class="p-4">
             <h3 class="text-semibold text-center mb-2">Image Preview</h3>
+            <img :src="filePreview" alt="" v-if="filePreview" class="bg-white p-1 border rounded">
           </div>
         </div>
       </div>
@@ -30,9 +33,33 @@
 </template>
 
 <script setup>
-    // get image preview
+  import { ref } from "vue";
 
-    // submit upload
+  const file = ref(null);
+  const filePreview = ref(null);
+  const error = ref(null);
 
-    // uploadImage connect  with upload API
+  // handleFileChange
+  const handleFileChange = (e) => {
+      file.value = e.target.files[0];
+      getImagePreviews(file.value);
+  }
+
+  // get image preview
+  const getImagePreviews = (image) => {
+    if (/\.(jpe?g|png)$/i.test(image.name) && image.size < 1000000) {
+        let reader = new FileReader();
+        reader.onloadend = (e) => {
+          filePreview.value = e.target.result;
+        };
+        reader.readAsDataURL(image);
+    } else {
+      error.value = "File is not supported for size bigger than 1 MB!";
+      
+    }
+  };
+
+  // submit upload
+
+  // uploadImage connect  with upload API
 </script>
